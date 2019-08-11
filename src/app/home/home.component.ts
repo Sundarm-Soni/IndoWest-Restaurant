03ccacd0f5cb6d7d7105dc460beb33ebedb2b49c
Sundarm Soni <sundarm.soni097@gmail.com>
2019-08-11 15:19:06 +0530

    Home and Menu updation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {DishService} from '../services/dish.service';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+import { Dish } from '../shared/dish';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  dish: Dish;
+  promotion: Promotion;
 
-  constructor() { }
+  constructor(private dishservice: DishService,private promotionservice: PromotionService) { }
 
   ngOnInit() {
+  this.dish = this.dishservice.getFeaturedDish();
+  this.promotion = this.promotionservice.getFeaturedPromotion();
   }
 
 }
